bfe39baa6e30318a4e7dd3e82f0dbb222caa9915
JamieTremaine <jamie.tremaine@students.plymouth.ac.uk>
2024-01-30 23:13:15 +0000

    grpc question client

--- src/main/java/com/helphi/HelphiConfiguration.java
+++ src/main/java/com/helphi/HelphiConfiguration.java
@@ -1,12 +1,31 @@
 package com.helphi;
 
+import com.helphi.question.api.grpc.QuestionServiceGrpc;
+import com.helphi.repository.PatientRepository;
+import com.helphi.svc.PatientService;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import net.devh.boot.grpc.client.inject.GrpcClientBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @Configuration
 @EnableJpaRepositories
+@GrpcClientBean(
+        clazz = QuestionServiceGrpc.QuestionServiceBlockingStub.class,
+        beanName = "blockingStub",
+        client = @GrpcClient("question-service")
+)
 public class HelphiConfiguration {
+
+    /* IDE can incorrectly say bean cannot be autowired. Ignore */
+    @Bean
+    @Autowired
+    PatientService petientService(PatientRepository patientRepository,
+                                  QuestionServiceGrpc.QuestionServiceBlockingStub blockingStub) {
+        return new PatientService(patientRepository, blockingStub);
+    }
 
 
 }
--- src/main/java/com/helphi/svc/PatientService.java
+++ src/main/java/com/helphi/svc/PatientService.java
@@ -1,13 +1,9 @@
 package com.helphi.svc;
 
 import com.helphi.api.user.Patient;
-import com.helphi.question.api.grpc.GetQuestionRequest;
-import com.helphi.question.api.grpc.Question;
-import com.helphi.question.api.grpc.QuestionServiceGrpc;
+import com.helphi.question.api.grpc.*;
 import com.helphi.repository.PatientRepository;
-
 import jakarta.persistence.EntityNotFoundException;
-import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,31 +13,29 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
-   // private final PatientRepository patientRepository;
-    @GrpcClient("question-service")
-    private QuestionServiceGrpc.QuestionServiceBlockingStub questionSvc;
+    private final PatientRepository patientRepository;
+    private final QuestionServiceGrpc.QuestionServiceBlockingStub questionSvc;
 
-    //@Autowired
-   // public PatientService(PatientRepository patientRepository, QuestionServiceGrpc.QuestionServiceBlockingStub questionSvc) {
-    //    this.patientRepository = patientRepository;
-   //     this.questionSvc = questionSvc;
-   // }
+    @Autowired
+    public PatientService(PatientRepository patientRepository, QuestionServiceGrpc.QuestionServiceBlockingStub questionSvc) {
+        this.patientRepository = patientRepository;
+        this.questionSvc = questionSvc;
+    }
     
-    public void getPatient(UUID patientId) throws IllegalArgumentException {
-
-      //  return this.patientRepository.findById(patientId);
+    public Optional<Patient> getPatient(UUID patientId) throws IllegalArgumentException {
+        return this.patientRepository.findById(patientId);
     }
 
-    public void addPatient(Patient patient) throws IllegalArgumentException {
-       // return this.patientRepository.save(patient);
+    public Patient addPatient(Patient patient) throws IllegalArgumentException {
+        return this.patientRepository.save(patient);
     }
 
-    public void updatePatient(Patient patient) throws IllegalArgumentException, EntityNotFoundException {
-        //return this.patientRepository.save(patient);
+    public Patient updatePatient(Patient patient) throws IllegalArgumentException, EntityNotFoundException {
+        return this.patientRepository.save(patient);
     }
 
-    public void deletePatient(UUID patientId) throws EntityNotFoundException {
-       // this.patientRepository.deleteById(patientId);
+    public void deletePatient(UUID patientId) throws IllegalArgumentException {
+        this.patientRepository.deleteById(patientId);
     }
 
     public void getPatientConditions() {
@@ -56,13 +50,15 @@ public class PatientService {
 
     }
 
-    public void getPatientCheckIns() {
-        System.out.println("CHECKING IN");
-        GetQuestionRequest request = GetQuestionRequest.newBuilder().setConditionId("CONDITION_ID").build();
-        Question result = this.questionSvc.getQuestion(request);
+    public void getPatientCheckInsForCondition(UUID userId, UUID conditionId) {
+        var request = GetUsersResponsesForConditionRequest.newBuilder()
+                .setUserId(userId.toString())
+                .setConditionId(conditionId.toString())
+                .build();
 
-        long id = result.getQuestionId();
-        System.out.println(id);
+        GetUserReponsesReply response = this.questionSvc.getUsersResponsesForCondition(request);
+
+        /*TODO return response */
 
     }
 
